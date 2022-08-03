f280480bfc99d33d93b2f638a56fd36c0ccaeba0
Phillip <philliplee515@gmail.com>
2022-08-03 15:56:50 +0900

    echo web

--- main.go
+++ main.go
@@ -1,148 +1,26 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
+	"scrapper/scrapper"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/labstack/echo/v4"
 )
 
-type extractedJob struct {
-	id       string
-	title    string
-	location string
-	summary  string
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
 }
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+func handleScrape(c echo.Context) string {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	return term
+}
 
 func main() {
-	var jobs []extractedJob
-	c := make(chan []extractedJob)
-	totalPages := getPages()
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 
-	for i := 0; i < totalPages; i++ {
-		go getPage(i, c)
-	}
-
-	for i := 0; i < totalPages; i++ {
-		extractedJob := <-c
-		jobs = append(jobs, extractedJob...)
-	}
-
-	writeJobs(jobs)
-	fmt.Println("Done, extracted", len(jobs))
-}
-
-func getPage(page int, mainC chan<- []extractedJob) {
-	var jobs []extractedJob
-	c := make(chan extractedJob)
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	fmt.Println("Requesting", pageURL)
-	res, err := http.Get(pageURL)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-
-	searchCards := doc.Find(".job_seen_beacon")
-	searchCards.Each(func(i int, s *goquery.Selection) {
-		go extractJob(s, c)
-	})
-
-	for i := 0; i < searchCards.Length(); i++ {
-		job := <-c
-		jobs = append(jobs, job)
-	}
-	mainC <- jobs
-}
-
-func extractJob(card *goquery.Selection, c chan<- extractedJob) {
-	id_path := card.Find(".jcs-JobTitle")
-	id, _ := id_path.Attr("data-jk")
-	title := cleanString(id_path.Find("a>span").Text())
-	location := cleanString(card.Find(".companyLocation").Text())
-	summary := cleanString(card.Find(".job-snippet").Text())
-
-	c <- extractedJob{
-		id:       id,
-		title:    title,
-		location: location,
-		summary:  summary}
-}
-
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
-	})
-
-	return pages
-}
-
-func writeJobs(jobs []extractedJob) {
-	wC := make(chan []string)
-
-	file, err := os.Create("jobs.csv")
-	checkErr(err)
-
-	w := csv.NewWriter(file)
-	defer w.Flush()
-
-	headers := []string{"Link", "Title", "Location", "Summary"}
-
-	wErr := w.Write(headers)
-	checkErr(wErr)
-
-	for _, job := range jobs {
-		go writeInCsv(job, wC)
-		jobSlice := <-wC
-		jwErr := w.Write(jobSlice)
-		checkErr(jwErr)
-	}
-
-	
-
-}
-
-func writeInCsv(job extractedJob, wC chan<- []string) {
-	wC <- []string{
-		"https://kr.indeed.com/viewjob?jk=" + job.id,
-		job.title,
-		job.location,
-		job.summary,
-	}
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Request failed with Status:", res.StatusCode)
-	}
-}
-
-func cleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
-}
+} 
